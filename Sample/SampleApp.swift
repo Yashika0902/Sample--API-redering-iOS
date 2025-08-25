1446c353db174f2c2b4596f24a76d3fb92b86370
Yashika0902 <sumati0902@gmail.com>
2025-08-25 23:38:54 +0530

    Initial Commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by Yashika Kushwaha on 25/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
